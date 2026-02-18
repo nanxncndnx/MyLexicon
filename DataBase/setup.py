5f7ec8608b63d28c02d519568c6bd59358951b27
nanxncndnx-glitch <azimitoofan66@gmail.com>
2026-02-18 13:17:57 +0100

    creating the database and two function to check the word definition and adding a new word if it's not exsisted already in the db

---
+++ DataBase/setup.py
@@ -0,0 +1,39 @@
+import sqlite3 as sql
+from main import WakeUpModel
+
+# Create connections =>
+conn = sql.connect("Storage.db")
+c = conn.cursor()
+
+# Creating the table for words and their definnitions =>
+c.execute(""" CREATE TABLE IF NOT EXISTS VAULT(
+            word_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT VARCHAR(30) UNIQUE, definition TEXT) """)
+
+c.execute(""" CREATE TABLE IF NOT EXISTS examples(
+            example_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word_id INTEGER NOT NULL, sentence TEXT NOT NULL,
+            FOREIGN KEY (word_id) REFERENCES VAULT(word_id) ON DELETE CASCADE) """)
+
+def AddingNewOne(word, definition):
+    conn = sql.connect("Storage.db")
+    c = conn.cursor()
+    try:
+        c.execute(""" INSERT INTO VAULT(word, definition) VALUES("{}", "{}"); """.format(word, definition))
+        conn.commit()
+        return definition
+    except Exception as e:
+        return f"this is the error we are getting: \n {e}"
+
+
+def GrabingTheMeaning(word):
+    conn = sql.connect("Storage.db")
+    c = conn.cursor()
+    definition = c.execute(f""" SELECT definition from VAULT WHERE word = '{word}'; """).fetchone()
+    
+    if definition is None:
+        meaning = WakeUpModel(word)
+        ans = AddingNewOne(word, meaning)
+        return ans
+    else:
+        return definition[0]
