686c23111bd13db8b2c858eb7d05559272236b16
nanxncndnx-glitch <azimitoofan66@gmail.com>
2026-02-18 13:14:06 +0100

    adding the function to call the api and get the meaning of the word from kimi-k2.5 model

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import openai
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+import os
+from dotenv import load_dotenv
 
 # importing all the tabs python files =>
 from Tabs import Glean
@@ -9,6 +13,25 @@ from header_style import (
     GLASS_DIVIDER_HTML,
     LOGO_HTML,
 )
+
+
+# Catching the answer from the kimi-k2.5 =>
+def WakeUpModel(word):
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    
+    client = openai.OpenAI(
+        base_url = "https://integrate.api.nvidia.com/v1",
+        api_key = API_KEY
+    )
+
+    response = client.chat.completions.create(
+        model="moonshotai/kimi-k2.5",
+        messages=[{"role": "user", "content": f"just define the word {word} very friendly and short like a translate in english no more and less information and talk just the meaning"}]
+    )
+
+    res = response.choices[0].message.content
+    return res
 
 # Logo of App =>
 st.markdown(LOGO_HTML, unsafe_allow_html=True)
@@ -20,7 +43,8 @@ navbar = option_menu(
     icons=['question-lg', 'database-fill-add', 'safe', 'hammer'], 
     default_index=0,
     orientation="horizontal",
-    styles=MENU_STYLES
+    styles=MENU_STYLES,
+    key="main_navbar"
 )
 
 # Glass Divider
